31077d6c850a22045e090fb0cf7a75fa60d64b68
Felipe Waku <felipe.koji2@gmail.com>
2016-01-05 15:28:13 -0200

    Projeto android sem interface

--- Documents/Jonatas/Onboard/app/src/main/java/com/example/taqtile/onboard/User.java
+++ Documents/Jonatas/Onboard/app/src/main/java/com/example/taqtile/onboard/User.java
@@ -1,6 +1,7 @@
 package com.example.taqtile.onboard;
 
 import java.util.*;
+import android.util.Log;
 
 
 /**
@@ -8,7 +9,7 @@ import java.util.*;
  */
 public class User {
     /*Classe de informações do usuário: nome, sobrenome e avatar */
-    public class info {
+    public class info extends HashMap<Integer, info> {
         public String first_name, last_name, avatar;
         public void set(String first_name, String last_name, String avatar){
             this.first_name = first_name;
@@ -17,13 +18,15 @@ public class User {
         }
     }
 
-    public int ViewCount;
     /*static info Info = new info("Joao","Carlos","http://queconceito.com.br/wp-content/uploads/2015/01/Emoticon.jpg");*/
     static ArrayList<HashMap<Integer, info>> lista;
     static String[] nome = {"Joao","Jose","Jaime","Carlos","Jhennifer","Camila","Stefanie","Marcos"};
     static String[] sobrenome = {"Silva","Costa","Lopez","Vargas","Buuck","Steuer","Ferreira","Gouveia"};
+    int [] ViewCount = new int[10];
+    public static final String USER_TAG = "User";
 
     public ArrayList<HashMap<Integer, info>> list(int Pagina){
+        Log.i(USER_TAG,"list");
         lista = new ArrayList<HashMap<Integer, info>>();
         int n1, n2;
         info random_info = new info();
@@ -32,17 +35,23 @@ public class User {
             n1 = r.nextInt(7);
             n2 = r.nextInt(7);
             random_info.set(nome[n1],sobrenome[n2],"http://queconceito.com.br/wp-content/uploads/2015/01/Emoticon.jpg");
+
+            lista.add(i,random_info);
         }
-        System.out.println(lista);
+        
         return lista;
     }
     public void incrementViewCount(int id){
-        this.ViewCount += 1;
+        Log.i(USER_TAG,"incrementViewCount");
+        ViewCount[id] += 1;
     }
     public void resetViewCount(int id){
-        this.ViewCount = 0;
+        Log.i(USER_TAG,"resetViewCount");
+        ViewCount[id] = 0;
     }
-    public int getViewCount(){
-        return this.ViewCount;
+    public int getViewCount(int id)
+    {
+        Log.i(USER_TAG,"getViewCount");
+        return ViewCount[id];
     }
 }
